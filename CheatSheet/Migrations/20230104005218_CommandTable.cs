242fc5c03f73a78ef2aa623c2cecf23311c8c126
gabrielkf <gabrielkf@gmail.com>
2023-01-03 21:54:21 -0300

    Adjust command model; create migration

---
+++ CheatSheet/Migrations/20230104005218_CommandTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CheatSheet.Migrations
+{
+    public partial class CommandTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Commands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Line = table.Column<string>(maxLength: 100, nullable: false),
+                    Description = table.Column<string>(maxLength: 250, nullable: false),
+                    Platform = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Commands", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Commands");
+        }
+    }
+}
--- CheatSheet/Models/Command.cs
+++ CheatSheet/Models/Command.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CheatSheet.Models
 {
     public class Command
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(100)]
         public string Line { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
         public string Description { get; set; }
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
