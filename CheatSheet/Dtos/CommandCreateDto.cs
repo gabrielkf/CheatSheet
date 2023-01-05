5dba00c755c5ddd50462e9250e235b6451e1dccc
gabrielkf <gabrielkf@gmail.com>
2023-01-05 12:52:45 -0300

    Make fields required in creation dto; now returns 400 status

--- CheatSheet/Dtos/CommandCreateDto.cs
+++ CheatSheet/Dtos/CommandCreateDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CheatSheet.Dtos
 {
     public class CommandCreateDto
     {
+        [Required]
+        [MaxLength(100)]
         public string Line { get; set; }
+        
+        [Required]
+        [MaxLength(250)]
         public string Description { get; set; }
+        
+        [Required]
         public string Platform { get; set; }
     }
 }
